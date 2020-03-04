aba27c4c7e931acd67fe6f7370c3772c6a4ef36d
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-04 18:35:44 +0000

    added icon into link

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Link = props => {
   return (
     <div>
-      <a href={props.url}>{props.text}</a>
-      <i class="fas fa-external-link-alt"></i>
+      <a href={props.url}>
+        {props.text} <i className="fas fa-external-link-alt"></i>
+      </a>
     </div>
   );
 };
