75d1006b082755d8db1421dd210c6279fa3acd78
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-04 17:32:22 +0000

    added input text fields to asset information

--- src/components/AssetInformation.jsx
+++ src/components/AssetInformation.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import InputText from "./InputText";
 
 const AssetInformation = () => {
-  return <div></div>;
+  return (
+    <div>
+      <InputText defaultValue="Asset name" />
+      <InputText defaultValue="Asset code" />
+      <InputText defaultValue="Max issuance amount" />
+    </div>
+  );
 };
 
 export default AssetInformation;
