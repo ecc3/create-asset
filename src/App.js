8501decb1890d1281df5070ef7aca477fa088f19
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-04 17:09:52 +0000

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
 import "./App.css";
+import { Router } from "@reach/router";
+import React, { Component } from "react";
+import AssetInformation from "./components/AssetInformation";
+import Advanced from "./components/Advanced";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <AssetInformation path="/" />
+          <Advanced path="advanced" />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
