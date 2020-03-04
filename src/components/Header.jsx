79c06dd600ca54b7a62ec62910ddb4cf4f101f21
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-04 18:36:32 +0000

    created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h2>Create asset</h2>
+      <i className="fas fa-times"></i>
+    </div>
+  );
+};
+
+export default Header;
