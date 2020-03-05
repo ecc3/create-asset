ea851f7b937aa17475d86a9f83ea769a90669ea3
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-05 12:14:03 +0000

    added styling to navigation links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import React, { Component } from "react";
 import AssetInformation from "./components/AssetInformation";
 import Advanced from "./components/Advanced";
 import Header from "./components/Header";
+import Tabs from "muicss/lib/react/tabs";
+import Tab from "muicss/lib/react/tab";
+import { navigate } from "@reach/router";
 
 class App extends Component {
   state = {
@@ -21,19 +24,21 @@ class App extends Component {
   };
 
   saveUserInput = (field, data) => {
-    this.setState({ [field]: data }, () => {
-      console.log(this.state, "state");
-    });
+    this.setState({ [field]: data });
   };
+
+  onChange(i, value, tab, ev) {
+    navigate(value);
+  }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <nav>
-          <Link to="/">1. Asset Information</Link>
-          <Link to="advanced">2. Advanced</Link>
-        </nav>
+        <Tabs onChange={this.onChange} defaultSelectedIndex={1}>
+          <Tab value="/" label="1. Asset Information"></Tab>
+          <Tab value="advanced" label="2. Advanced"></Tab>
+        </Tabs>
         <Router>
           <AssetInformation path="/" saveUserInput={this.saveUserInput} />
           <Advanced
