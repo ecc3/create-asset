24595b801ade5cf30fd05dca1fcef257c89242c1
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-05 11:34:43 +0000

    refactored add file component to use dropzone library

--- src/components/AddFile.jsx
+++ src/components/AddFile.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useDropzone } from "react-dropzone";
 
 const AddFile = props => {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
   return (
-    <label>
-      {props.label}
-      <input type="file" />
-    </label>
+    <section className="container">
+      <p>{props.label}</p>
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <h4>Drag a file here or click to browse</h4>
+        <p>{props.smallText}</p>
+      </div>
+      <aside>
+        <ul>{files}</ul>
+      </aside>
+    </section>
   );
 };
 
