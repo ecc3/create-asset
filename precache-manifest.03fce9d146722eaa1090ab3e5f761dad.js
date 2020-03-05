410a6c8706f64bf00f81678043cf3b990077fbb2
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-05 13:16:27 +0000

    Updates

---
+++ precache-manifest.03fce9d146722eaa1090ab3e5f761dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508acaf73988de582e0b40ce861da7ba",
+    "url": "/create-asset/index.html"
+  },
+  {
+    "revision": "13dfff496161e974b90b",
+    "url": "/create-asset/static/css/main.6390296e.chunk.css"
+  },
+  {
+    "revision": "82e5510152123939c55e",
+    "url": "/create-asset/static/js/2.a659fbab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/create-asset/static/js/2.a659fbab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13dfff496161e974b90b",
+    "url": "/create-asset/static/js/main.097729d4.chunk.js"
+  },
+  {
+    "revision": "2c08cbb4a86a05f04aed",
+    "url": "/create-asset/static/js/runtime-main.cc1b95f1.js"
+  }
+]);
