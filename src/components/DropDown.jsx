0fdf293a4a0539c88ab7a2f530b64a769af52ef3
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-05 10:47:52 +0000

    added default value to dropdown list

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 const DropDown = props => {
+  const handleBlur = event => {
+    console.log(event.target.checked);
+    props.saveUserInput(props.name, event.target.checked);
+  };
   return (
     <select>
+      <option selected disabled hidden>
+        {props.defaultValue}
+      </option>
       {props.options.map(passedOption => {
         return (
           <option value={passedOption} key={passedOption}>
