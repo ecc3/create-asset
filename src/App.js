9e3514680d807b25921720ecac4de1a6203b5520
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-05 10:24:23 +0000

    added header to App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Router, Link } from "@reach/router";
 import React, { Component } from "react";
 import AssetInformation from "./components/AssetInformation";
 import Advanced from "./components/Advanced";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <nav>
           <Link to="/">1. Asset Information</Link>
           <Link to="advanced">2. Advanced</Link>
