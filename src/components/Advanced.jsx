6046589f9b6a318c34d7960d84463f450ed5cda1
ecc3 <liz3.33zie@yahoo.co.uk>
2020-03-05 10:58:27 +0000

    properties for saving inputted data passed to components

--- src/components/Advanced.jsx
+++ src/components/Advanced.jsx
@@ -2,19 +2,37 @@ import React from "react";
 import Checkbox from "./Checkbox";
 import InputText from "./InputText";
 import AddFile from "./AddFile";
+import SubmitButton from "./SubmitButton";
 import Link from "./Link";
 
-const Advanced = () => {
+const Advanced = props => {
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
   return (
-    <form action="">
-      <Checkbox checkName="I do not want to make additional issuance later" />
+    <form onSubmit={handleSubmit}>
+      <Checkbox
+        checkName="I do not want to make additional issuance later"
+        name="makeAdditionalIssuance"
+        saveUserInput={props.saveUserInput}
+      />
       <Link
         url="https://www.dictionary.com/browse/pre-issuance"
         text="Learn more about pre-issuance"
       />
-      <InputText defaultValue="Pre-issuance asset signer ID" />
-      <InputText defaultValue="Initial pre-issued amount" />
+      <InputText
+        defaultValue="Pre-issuance asset signer ID"
+        name="assetSignerID"
+        saveUserInput={props.saveUserInput}
+      />
+      <InputText
+        defaultValue="Initial pre-issued amount"
+        name="initialAmount"
+        saveUserInput={props.saveUserInput}
+      />
       <AddFile label="Upload terms" />
+      <SubmitButton text="Create request" />
     </form>
   );
 };
